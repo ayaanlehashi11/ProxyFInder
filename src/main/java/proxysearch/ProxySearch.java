2d5eaf526013a3b6ca17823dbc6949b5624237b9
ayaanle <ayaanlemohamed540@gmail.com>
2022-08-24 18:53:27 +0300

    Proxy

--- src/main/java/proxysearch/ProxySearch.java
+++ src/main/java/proxysearch/ProxySearch.java
@@ -1,4 +1,7 @@
 package proxysearch;
-
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.net.http.HttpRequest;
+import java.net.HttpURLConnection;
 public class ProxySearch {
 }
